b78fcedb611ba255ad99abb64a98a144edf764b4
Caio Geraldo Botelho Valiati <caiogeraldo@comigo.com.br>
2025-05-09 11:09:44 -0300

    .

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.ts
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  }
+};
---
+++ src/App.test.tsx
@@ -0,0 +1,7 @@
+// src/App.test.tsx
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("renderiza sem falhas", () => {
+  render(<App />);
+});
