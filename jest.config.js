c69155874a751ee745c5503479c34a23290d0436
Caio Geraldo Botelho Valiati <caiogeraldo@comigo.com.br>
2025-05-09 11:26:27 -0300

    .

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,10 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  }
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+    '^react-router-dom$': '<rootDir>/node_modules/react-router-dom',
+  },
 };
